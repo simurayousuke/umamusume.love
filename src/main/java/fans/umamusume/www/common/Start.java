92b191bb2d524a3bb4590b0001f987480038617f
庄庄庄庄庄 <yzz0427@vip.qq.com>
2021-03-23 23:12:34 +0900

    加入评论留言功能

--- src/main/java/fans/umamusume/www/common/Start.java
+++ src/main/java/fans/umamusume/www/common/Start.java
@@ -4,7 +4,7 @@ import com.jfinal.server.undertow.UndertowServer;
 
 public class Start {
 
-    public static final String version = "0.5.1 Beta";
+    public static final String version = "0.6.0 Beta";
     public static boolean devMode;
 
     public static void main(String[] args) {
